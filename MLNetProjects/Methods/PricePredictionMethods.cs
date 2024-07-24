90bc05aa610a7897a65896185ab1904fb38eaf3a
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-07-23 21:22:57 -0300

    progresso

---
+++ MLNetProjects/Methods/PricePredictionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLNetProjects.Methods
+{
+    public class PricePredictionMethods
+    {
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Public Methods
+
+        /// <summary>
+        /// Método base usado para carregar o modelo de linguagem para previsão de preços
+        /// </summary>
+        public void LoadData()
+        {
+
+        }
+
+        #endregion Public Methods
+
+    }
+}
